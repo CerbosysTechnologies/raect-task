593325d8a046b1c0dc61c65d44f38a832a93ab3d
Lalit <93573082+CerbosysTechnologies@users.noreply.github.com>
2024-03-18 19:14:04 +0530

    Chanages

---
+++ src/Componets/Dashboard/Chart2/MyLineChart.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+import salesData from '../../../jsonData/salesData.json';
+
+const MyLineChart = () => {
+  const [lineVisibility, setLineVisibility] = useState({
+    web_sales: true,
+    offline_sales: true,
+  });
+
+  const handleLegendClick = (entry) => {
+    const { dataKey } = entry;
+
+    setLineVisibility((prevState) => ({
+      ...prevState,
+      [dataKey]: !prevState[dataKey],
+    }));
+  };
+  return (
+    <>
+      <div
+        style={{
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+          borderRadius: '0.5rem',
+          padding: '1rem',
+          background: '#fff',
+        }}
+      >
+        <h1 className="dashboard-head">Customers by Device</h1>
+
+        <br />
+        <ResponsiveContainer width="100%" height={300}>
+          <LineChart data={salesData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend onClick={handleLegendClick} />
+            {lineVisibility.web_sales && (
+              <Line
+                type="monotone"
+                dataKey="web_sales"
+                stroke="#8884d8"
+                name="Web Sales"
+              />
+            )}
+            {lineVisibility.offline_sales && (
+              <Line
+                type="monotone"
+                dataKey="offline_sales"
+                stroke="#82ca9d"
+                name="Offline Sales"
+              />
+            )}
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </>
+  );
+};
+
+export default MyLineChart;
--- src/Componets/Dashboard/Dashboard.jsx
+++ src/Componets/Dashboard/Dashboard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from '@mui/material';
+import Barchart from './Barchart/Barchart'; // Assuming you have a Barchart component
+import MyLineChart from './Chart2/MyLineChart'; // Assuming you have a MyLineChart component
 
-const Dashboard = () => {
+const ChartsContainer = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={9}>
+          <Barchart />
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <MyLineChart />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default Dashboard
+export default ChartsContainer;
