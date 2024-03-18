20e900645a8fb21bd837306c865c21b00ab6f0ab
Vinayadav08 <dev16.cerbosys@gmail.com>
2024-03-18 15:19:31 +0530

    pus

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './auth/Login';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import Dashboard from './Componets/Dashboard';
+import Dashboard from './Componets/Dashboard/Dashboard';
+import Navbar from './Componets/Navbar/Navbar';
 
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <BrowserRouter> {/* Use BrowserRouter instead of Router */}
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter> 
+   </>
   );
 }
 
